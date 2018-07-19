6a8522231725a7c18fe04a2d074debad52630fea
tadeuaugusto <tadeuaugusto@gmail.com>
2018-07-18 23:38:12 -0300

    modified after docker automated build

--- docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -4,7 +4,7 @@ public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
 		for(int i = 0 ; i < 320; i++){
-			System.out.println("VAI CORINTHIANS!! ("+i+")");
+			System.out.println("CORINTHIANS Campeao dos Campeoes!! ("+i+")");
 			Thread.sleep(1000);
 		}
 	}
